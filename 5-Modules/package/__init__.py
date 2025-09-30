a766e97c2b5120c739d119931bdc28504e78f50d
unknown <prasetyahenry450@gmail.com>
2025-09-30 17:49:52 +0700

    Initial commit

---
+++ 5-Modules/package/__init__.py
@@ -0,0 +1,6 @@
+'''file ini berfungsi untuk membuat modul yang bersifat sesuai dengan apa yang sedang dibutuhkan
+Wajib harus ada file spesial __init__.py bila menggunakan package
+
+sebagai contohnya disini adalah maths.py
+
+'''
---
+++ 5-Modules/test.py
@@ -0,0 +1,6 @@
+from package.maths import* # mengambil seluruh fungsi yang ada di file maths
+from package.subpackages.mult import multiply # Mengambil fungsi 'multiply' dari file 'mult.py' yang berada di dalam folder 'package/subpackages'.
+
+print(addition(2,3))
+print(substraction(4,3))
+print(multiply(4,5))
